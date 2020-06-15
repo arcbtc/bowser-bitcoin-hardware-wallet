d509ed882925541c0ffb85f6271fb0087ac2a004
Arc <33088785+arcbtc@users.noreply.github.com>
2020-06-15 13:54:39 +0100

    Update decoy.h

--- bowser/decoy.h
+++ bowser/decoy.h
@@ -160,7 +160,7 @@ void decoysetup(){
   M5.Lcd.drawBitmap(0, 0, 320, 240, (uint8_t *)GameImg_map);
   
   int starttime = millis();
-  while (millis() - starttime < 1000){
+  while (millis() - starttime < 500){
     if (M5.BtnC.wasReleased()){decoy = false; M5.Lcd.fillScreen(BLACK);}
     M5.update(); 
   }
